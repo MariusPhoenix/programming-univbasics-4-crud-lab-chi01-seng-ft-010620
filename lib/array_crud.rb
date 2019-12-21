b594998f9d793c18bb5b9deb9f87a80e432bf618
Marius Phoenix <phoenixzenomarius@gmail.com>
2019-12-21 02:56:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  array[index_numer]
+def retrieve_element_from_index(array, 2)
+  
 end
 
 def retrieve_first_element_from_array(array)
